8693f2647b5695cade2b448727ba3718b5d9d5be
Deepinder Singh <deepindersingh012@gmail.com>
2023-01-29 11:43:21 +0530

    about + trainers

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import aboutImg from '../../images/hero.jpg'
+import aboutImg from '../../images/about.jpg'
+import professionalsImg from '../../images/professionals.jpg'
 function About() {
   return (
     <>
       <section className='wt-section pb-0' id='about'>
         <div className='container'>
           <div className='row justify-content-between align-items-center'>
-            <div className='col-md-5'>
+            <div className='col-lg-5'>
               <img width={'90%'} src={aboutImg} alt='' className='rounded-lg' />
             </div>
-            <div className='col-md-7'>
-              <h2 className='mb-4'>About Us</h2>
-              <p className='text-muted lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga eum asperiores commodi unde. Expedita a sed quidem aut quod, aperiam cupiditate consequatur nihil, vero, maiores deleniti necessitatibus nesciunt incidunt minima?</p>
+            <div className='col-lg-7'>
+              <h2 className='mb-4 mt-3'>About Us</h2>
+              <p className='text-muted lorem'>FitnessHub is a fitness centre that changes your perspective about fitness. We believe in building a stronger you, through personalization and innovation in your everyday goals. We are the gym filled with Luxurious amenities, stunning decor and cutting edge equipment.</p>
             </div>
           </div>
         </div>
@@ -22,14 +23,13 @@ function About() {
         <div className='container'>
           <div className='row justify-content-between align-items-center'>
             
-            <div className='col-md-7'>
-              <h2 className='mb-4'>Professional Team</h2>
-              <p className='text-muted lorem'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga eum asperiores commodi unde. Expedita a sed quidem aut quod, aperiam cupiditate consequatur nihil, vero, maiores deleniti necessitatibus nesciunt incidunt minima?</p>
+            <div className='col-lg-7'>
+              <h2 className='mb-4'>Professional Trainers</h2>
+              <p className='text-muted lorem'>As a member of FitnessHub, you will enjoy access to the best coaches, personal trainers, and group exercise instructors. That means access to motivating and knowledgeable experts dedicated to your success. Whatever your goal or experience level, our wide range of fitness classes and offerings can help you reach your goals.</p>
             </div>
 
-            {/* TODO second about image */}
-            <div className='col-md-5'>
-              <img width={'90%'} src={aboutImg} alt='' className='rounded-lg' />
+            <div className='col-lg-5 mt-3'>
+              <img width={'90%'} src={professionalsImg} alt='' className='rounded-lg' />
             </div>
           </div>
         </div>
